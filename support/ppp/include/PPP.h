6e13c757ac97af7670a8aae8f1bbb3cacc5aa697
FeignClaims <www4112379@163.com>
2024-10-04 22:27:09 +0800

    feat: remove ppp checked library, use sanitizers instead

--- support/ppp/include/PPP.h
+++ support/ppp/include/PPP.h
@@ -9,9 +9,4 @@
 import PPP;
 using namespace PPP;
 using namespace std;
-
-// disgusting macro hack to guarantee range checking for []:
-#define vector Checked_vector
-#define string Checked_string
-#define span   Checked_span
 // NOLINTEND
--- support/ppp/include/PPPheaders.h
+++ support/ppp/include/PPPheaders.h
@@ -24,9 +24,4 @@
 
 using namespace std;
 using namespace PPP;
-
-// disgusting macro hack to get a range checking:
-#define vector Checked_vector
-#define string Checked_string
-#define span   Checked_span
 // NOLINTEND
