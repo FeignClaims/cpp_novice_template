97559e805d8a434d151b2b2e698f4b707ab44ff8
FeignClaims <www4112379@163.com>
2024-10-24 20:20:39 +0800

    feat: include more standard libraries in PPPheaders.h

--- support/ppp/include/PPPheaders.h
+++ support/ppp/include/PPPheaders.h
@@ -3,21 +3,30 @@
 
 // NOLINTBEGIN
 #include <algorithm>      // IWYU pragma: export
+#include <array>          // IWYU pragma: export
+#include <cmath>          // IWYU pragma: export
 #include <cstddef>        // IWYU pragma: export
 #include <cstdint>        // IWYU pragma: export
+#include <cstdlib>        // IWYU pragma: export
 #include <exception>      // IWYU pragma: export
+#include <forward_list>   // IWYU pragma: export
 #include <fstream>        // IWYU pragma: export
 #include <functional>     // IWYU pragma: export
+#include <iomanip>        // IWYU pragma: export
+#include <ios>            // IWYU pragma: export
 #include <iostream>       // IWYU pragma: export
+#include <limits>         // IWYU pragma: export
 #include <list>           // IWYU pragma: export
 #include <map>            // IWYU pragma: export
 #include <memory>         // IWYU pragma: export
 #include <random>         // IWYU pragma: export
+#include <regex>          // IWYU pragma: export
 #include <set>            // IWYU pragma: export
 #include <span>           // IWYU pragma: export
 #include <sstream>        // IWYU pragma: export
 #include <stdexcept>      // IWYU pragma: export
 #include <string>         // IWYU pragma: export
+#include <type_traits>    // IWYU pragma: export
 #include <unordered_map>  // IWYU pragma: export
 #include <vector>         // IWYU pragma: export
 
