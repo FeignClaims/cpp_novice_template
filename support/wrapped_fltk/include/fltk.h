c31fd6b223f632588e47506e4a40bc5be89e5758
FeignClaims <www4112379@163.com>
2024-10-24 18:02:39 +0800

    feat: add ppp's graph_lib as support

--- support/wrapped_fltk/include/fltk.h
+++ support/wrapped_fltk/include/fltk.h
@@ -2,10 +2,6 @@
 #define FLTK_GUARD
 
 // NOLINTBEGIN
-#ifndef CPP_NOVICE_FLTK_FOUND
-#  error You haven't install fltk. Please install fltk and reconfigure the project according to README.pdf. (你没有安装 fltk, 请阅读 请读我.pdf 安装 fltk 并重新配置项目.)
-#endif
-
 #include "FL/Enumerations.H"   // IWYU pragma: export
 #include "FL/Fl.H"             // IWYU pragma: export
 #include "FL/Fl_Button.H"      // IWYU pragma: export
